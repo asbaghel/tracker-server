51c40c6c1d51b67671985d1dcedc2f61dd35bb34
asbaghel <abhisheksbaghel99@gmail.com>
2020-06-21 17:59:29 +0530

    commit 2

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ app.get("/", requireAuth, (req, res) => {
   res.send(`Your email ${req.user.email}`);
 });
 
-const PORT = 3000;
+const PORT = 3010;
 app.listen(PORT, () => {
   console.log(`listening on node ${PORT} `);
 });
